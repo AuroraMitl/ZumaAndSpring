e24efe64e37e9ca19aa3bf3798f549ee3ab0cf16
Pomaxa <pomarot@gmail.com>
2021-04-14 00:18:26 +0300

    add pet through RequestMapping and ModelAndView

--- src/main/java/com/aurora/petclinic/controllers/PetController.java
+++ src/main/java/com/aurora/petclinic/controllers/PetController.java
@@ -6,7 +6,10 @@ import com.aurora.petclinic.services.ClientService;
 import com.aurora.petclinic.services.PetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -33,24 +36,36 @@ public class PetController {
 //    }
 
     @GetMapping("/addPet")
-    public String showAddPetForm(@ModelAttribute("clientId") @Valid int clientId, Model model){
-        model.addAttribute("pet",new Pet());
-        model.addAttribute("client", clientService.findById(clientId));
+    public String showAddPetForm(@RequestParam("clientId") @Valid int clientId, Model model){
+        model.addAttribute("pet", new Pet());
+        model.addAttribute("clientId", clientId);
         return "editPetView";
     }
 
-    @PostMapping("/addPet")
-    public String savePet(@ModelAttribute("pet") @Valid Pet pet, Model model){
+/*    @PostMapping("/addPet")
+    public String savePet(@ModelAttribute("pet") @Valid Pet pet, @RequestParam("clientId") @Valid int clientId, Model model){
+        Client client = clientService.findById(clientId);
+        pet.setClient(client);
         petService.savePet(pet);
-        Client client = clientService.findById(pet.getClient().getId());
+        pet = null;
         model.addAttribute("client",client);
         return "editClientView";
+    }*/
+
+    @PostMapping("/addPet")
+    public ModelAndView savePet(@ModelAttribute("pet") @Valid Pet pet, @RequestParam("clientId") @Valid int clientId, ModelMap model){
+        Client client = clientService.findById(clientId);
+        pet.setClient(client);
+        petService.savePet(pet);
+        model.addAttribute("id", client.getId());
+        return new ModelAndView("redirect:/clients/showFormForClientUpdate", model);
     }
 
     @PostMapping("/showFormForPetUpdate")
     public String showFormForEditPet(@RequestParam("id") @Valid int id, Model model) {
         Pet pet = petService.findById(id);
         model.addAttribute("pet", pet);
+        model.addAttribute("clientId", pet.getClient().getId());
         return "editPetView";
     }
 
